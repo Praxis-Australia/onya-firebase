c962e75759d7034b23c4dba108bfe7ec6202680e
Brian Lee <briancmlee23@gmail.com>
2022-12-19 01:47:03 +1100

    Fix timestamp for userCreated field

--- functions/index.js
+++ functions/index.js
@@ -85,7 +85,7 @@ const createUser = async (uid, phoneNumber) => {
         const writeResult = await admin.firestore().collection('users').doc(uid).set({
             ...templateUserDoc,
             uid: uid,
-            userCreated: admin.database.ServerValue.TIMESTAMP,
+            userCreated: admin.firestore.Timestamp.now()
         })
 
         functions.logger.log(`Success creating user ${uid}`);
@@ -133,30 +133,3 @@ exports.createUserBasiq = functions.auth.user().onCreate(async user => {
         functions.logger.error(`Error updating user ${uid} with Basiq details`, err)
     }
 })
-
-// exports.
-
-// exports.addMessage = functions.https.onRequest(async (req, res) => {
-//     // Grab the text parameter.
-//     const original = req.query.text;
-//     // Push the new message into Firestore using the Firebase Admin SDK.
-//     const writeResult = await admin.firestore().collection('messages').add({original: original});
-//     // Send back a message that we've successfully written the message
-//     res.json({result: `Message with ID: ${writeResult.id} added.`});
-// });
-
-// exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-//     .onCreate((snap, context) => {
-//     // Grab the current value of what was written to Firestore.
-//     const original = snap.data().original;
-
-//     // Access the parameter `{documentId}` with `context.params`
-//     functions.logger.log('Uppercasing', context.params.documentId, original);
-
-//     const uppercase = original.toUpperCase();
-
-//     // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//     // writing to Firestore.
-//     // Setting an 'uppercase' field in Firestore document returns a Promise.
-//     return snap.ref.set({uppercase}, {merge: true});
-// });
