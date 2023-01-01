6bc553a6d203f5d7ad379699d0f5ec29fd49f4f1
Brian Lee <briancmlee23@gmail.com>
2023-01-01 19:47:38 +1100

    Modularise and add basiq refresh functionality

--- functions/index.js
+++ functions/index.js
@@ -1,76 +1,11 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { URLSearchParams } = require('url');
-const fetch = require('node-fetch');
 const fs = require('fs');
+const basiqApi = require('./basiqApi');
 
 let templateUserDoc = JSON.parse(fs.readFileSync('template_user_doc.json'));
 
 admin.initializeApp();
-
-const getBasiqToken = async () => {
-    const basiqDoc = await admin.firestore().collection('env').doc('basiqToken').get();
-
-    try {
-        if (basiqDoc.exists) {
-            const basiqDocData = basiqDoc.data();
-            if (basiqDocData.access_token && basiqDocData.last_updated && new Date().getTime() - basiqDocData.last_updated < 3300 * 1000) {
-                functions.logger.log("returning cached basiq token")
-                return basiqDocData.access_token;
-            } else {
-                return await refreshBasiqToken();
-            }
-        } else {
-            return await refreshBasiqToken();
-        }
-    } catch (e) {
-        functions.logger.error(e);
-    }
-};
-
-const refreshBasiqToken = async () => {
-    functions.logger.log("fetching new basiq token")
-    const encodedParams = new URLSearchParams();
-    encodedParams.set('scope', 'SERVER_ACCESS');
-
-    const options = {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'basiq-version': '3.0',
-            'content-type': 'application/x-www-form-urlencoded',
-            Authorization: `Basic ${process.env.BASIQ_API_KEY}`
-        },
-        body: encodedParams
-    };
-
-    const url = 'https://au-api.basiq.io/token';
-    const res = await fetch(url, options);
-    const json = await res.json();
-
-    let writeResult = await admin.firestore().collection('env').doc('basiqToken').set({
-        access_token: json.access_token,
-        last_updated: new Date().getTime()
-    })
-    return json.access_token;
-}
-
-const createAuthLink = async (basiqUid) => {
-    const url = `https://au-api.basiq.io/users/${basiqUid}/auth_link`;
-    const options = {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            authorization: `Bearer ${await getBasiqToken()}`
-        }
-    };
-
-    const res = await fetch(url, options);
-    const json = await res.json();
-
-    return json.links.public;
-}
 
 const createUser = async (uid) => {
     try {
@@ -89,39 +24,19 @@ const createUser = async (uid) => {
 
 exports.createUser = functions.auth.user().beforeCreate(async (user, context) => {
     const { uid } = user;
-    return createUser(uid);
+    return await createUser(uid);
 })
 
-exports.createUserBasiq = functions.auth.user().onCreate(async user => {
-    const { uid, phoneNumber } = user;
+exports.createBasiqUser = functions.https.onCall(async (data, context) => {
+    const { uid } = context.auth;
+    const phoneNumber = context.auth.token.phone_number;
+    const { firstName, lastName } = data;
+    const result = await basiqApi.createBasiqUser(uid, phoneNumber, firstName, lastName);
+    return result;
+})
 
-    try {
-        const options = {
-            method: 'POST',
-            headers: {
-                accept: 'application/json',
-                'content-type': 'application/json',
-                authorization: `Bearer ${await getBasiqToken()}`
-            },
-            body: JSON.stringify({
-                mobile: phoneNumber,
-            })
-        };
-    
-        const url = 'https://au-api.basiq.io/users';
-        const res = await fetch(url, options);
-        const json = await res.json();
-    
-        return await admin.firestore().collection('users').doc(uid).update({
-            basiq: {
-                "accountNames": [],
-                "availableAccounts": [],
-                "connectionIds": [],
-                "uid": json.id,
-                "consentLink": await createAuthLink(json.id)
-            },
-        });
-    } catch (err) {
-        functions.logger.error(`Error updating user ${uid} with Basiq details`, err)
-    }
+exports.refreshUserBasicInfo = functions.https.onCall(async (data, context) => {
+    const { uid } = context.auth;
+    const result = await basiqApi.refreshUserBasicInfo(uid);
+    return result;
 })
