9a2e9d84f454038312e5c895556b832ba3f896bf
Brian Lee <briancmlee23@gmail.com>
2023-01-12 16:48:16 +1100

    Add roundup util functions

--- functions/src/utils/roundup.ts
+++ functions/src/utils/roundup.ts
@@ -1,4 +1,15 @@
+import { Transaction } from "../api/basiq/schema";
+import { RoundupConfigEnabled } from "../models/User";
 
-const roundupBy = (amount: number, roundTo: number): number => {
+export const roundupBy = (amount: number, roundTo: number): number => {
   return roundTo - (amount % roundTo);
+}
+
+export const getTotalRoundupAmount = (transactionList: Transaction[], roundupConfig: RoundupConfigEnabled): number => {
+  return transactionList.reduce((total, transaction) => {
+    if (transaction.direction === 'credit') return total;
+    if (transaction.account != roundupConfig.watchedAccountId) return total;
+    
+    return total + roundupBy(Number.parseFloat(transaction.amount), roundupConfig.roundTo);
+  }, 0)
 }
